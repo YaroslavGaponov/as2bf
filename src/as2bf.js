f5203eaa5d2b56d37dea82818f002103675fdcb3
Yaroslav Gaponov <yaroslavgaponov@pl9996glygapon.home>
2021-01-31 22:56:04 +0100

    chore: binary

--- src/as2bf.js
+++ src/as2bf.js
@@ -2,31 +2,31 @@ const fs = require('fs');
 const Translator = require('./translator');
 const parse = require('./parser').parse;
 
-function log(s) { process.stdin.write(s); }
+function log(s) { process.stdout.write(s); }
 
-log(`[as2bf] Assembler to Brainfuck traslator [https://github.com/YaroslavGaponov]`);
+log(`as2bf Assembler to Brainfuck traslator [https://github.com/YaroslavGaponov/as2bf]`);
 switch (process.argv.length) {
     case 3:
     case 4:
         log('\nloading...');
         const program = fs.readFileSync(process.argv[2], 'utf8');
-        log('ok');
+        log('👍🏻');
         log('\nparsing...');
         const assembler = parse(program);
-        log('ok');
-        log('\n--- start ---\n'+assembler.toString()+'\n--- end ---');
+        log('👍🏻');
+        log('\n--- start ---\n' + assembler.toString() + '\n--- end ---');
         log('\ntranslating...');
         const translator = new Translator(assembler);
         const brainfuck = translator.trasform();
-        log('ok');
+        log('👍🏻');
         const result = brainfuck.toString();
         log(`\nresult is ${result.length} bytes`)
         if (process.argv[3]) fs.writeFileSync(process.argv[3], result);
-        else console.log(result);
-        console.log('\ndone');
-        break;
-    default: 
-        log('\nhelp: as2bf source {target}');
-        break;
+        else log('\n' + result);
+        log('\ndone');
+        process.exit(0);
+    default:
+        log('\n👉🏻 help: as2bf source {target}');
+        process.exit(-1);
 }
 
